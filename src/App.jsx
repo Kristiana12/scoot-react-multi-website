7d56be646a89811d35e594d394ef1eededb11ba7
Kristiana12 <kristiana.1992@hotmail.com>
2023-06-11 14:32:42 +0200

    remove comment

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ function App() {
         animate="visible"
       >
         {location.pathname === '/' ? <Home /> : <Outlet />}
-        {console.log(location.pathname)}
       </motion.main>
       {location.pathname === '/404' ? null : <Footer />}
     </ThemeProvider>
